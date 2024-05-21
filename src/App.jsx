c1c244c8eb310601907b320a7e384d1e3d4698b6
André Pires Nobre <andre.pires.nobre@gmail.com>
2024-05-20 21:23:32 -0300

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+
+
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
