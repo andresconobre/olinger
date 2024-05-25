d0fb0dd41de41426488a18328891d3f7587e0946
André Pires Nobre <andre.pires.nobre@gmail.com>
2024-05-25 02:05:57 -0300

    Animação de alteração de cor no hover Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Button = ({ children, to, arrow }) => {
   return (
-    <Link to={to} className='bg-blue-olinger-c9 py-2 px-4 flex items-center gap-2 font-bold text-sm text-blue-olinger-c1 rounded'>
+    <Link to={to} className='bg-blue-olinger-c9 hover:bg-blue-olinger-c10 transition-colors py-2 px-4 flex items-center gap-2 font-bold text-sm text-blue-olinger-c1 rounded'>
         {children}
         {arrow ? <img src='/img/arrow-right-icon.svg' alt='Ícone de seta para direita' className='w-4 h-4 object-contain'/> : null}
     </Link>
