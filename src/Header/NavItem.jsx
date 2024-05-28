3298c7b1a7381f4fd42210ed59a2e797b77f77ba
André Pires Nobre <andre.pires.nobre@gmail.com>
2024-05-27 23:25:10 -0300

    Transição mais suave NavItem

--- src/Header/NavItem.jsx
+++ src/Header/NavItem.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const NavItem = ({ children, to, className, ...props }) => {
   return (
-    <li className={`text-blue-olinger-c12 text-sm rounded hover:font-bold hover:bg-blue-olinger-c3 ${className}`} {...props}>
+    <li className={`text-blue-olinger-c12 text-sm rounded hover:font-bold hover:bg-blue-olinger-c3 transition-all ${className}`} {...props}>
         <Link to={to} className='py-2 px-4 inline-block'>{children}</Link>
     </li>
   )
